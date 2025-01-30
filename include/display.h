f3f7b5d6465ea701a97a270d511653349d8ec7eb
Williampasset <william.passet@student.junia.com>
2025-01-30 09:40:59 +0100

    feat(test) - test sin cos movement

--- include/display.h
+++ include/display.h
@@ -1,6 +1,8 @@
 #include "battle_c.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include <stdbool.h>
 
 void displayRadarData(BC_List *radarData);
 void displayMapData(BC_WorldInfo world_info);
+void displayPlayerData(BC_PlayerData data);
--- src/main.c
+++ src/main.c
@@ -1,30 +1,52 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "math.h"
+#include "unistd.h"
 #include "battle_c.h"
 #include "display.h"
 
+#define SPEED 5
+#define DELAY 100000
+#define FIRE_RATE 5
+
 int main(int argc, char *argv[])
 {
-  BC_Connection *conn = bc_connect("5.135.136.236", 8080);
+    BC_Connection *conn = bc_connect("5.135.136.236", 8080);
 
-  BC_WorldInfo world_info = bc_get_world_info(conn);
+    BC_WorldInfo world_info = bc_get_world_info(conn);
+    BC_PlayerData data = bc_get_player_data(conn);
 
-  BC_PlayerData data = bc_get_player_data(conn);
+    double angle = 0;
+    int counter = 0;
+    int direction = 1;
 
-  BC_List *list = bc_radar_ping(conn);
+    while (true) {
+        data = bc_get_player_data(conn);
 
-  displayMapData(world_info);
-  displayPlayerData(data);
-  displayRadarData(list);
-  
-  // while(data.position.y > 0 && data.position.y < 100){
-  //   data = bc_get_player_data(conn);
-  //   bc_set_speed(conn, 0, -1, 0);
+        if (data.position.x <= 20) {
+            direction = 1; 
+            angle = 0;
+        } else if (data.position.x >= 80) {
+            direction = -1;
+            angle = 0;      
+        }
 
-  //   bc_shoot(conn, 180);
-  // }
-  
-  bc_disconnect(conn);
+        double vx = cos(angle / 10);
+        double vy = direction * SPEED * sin(angle / 10);
 
-  return EXIT_SUCCESS;
+        bc_set_speed(conn, 1, vy, 0);
+
+        angle += 1;
+
+        if (counter % FIRE_RATE == 0) {
+            double fire_angle = angle * 180 / (M_PI * 10);
+            bc_shoot(conn, fire_angle);
+        }
+
+        counter++;
+        usleep(DELAY);
+    }
+
+    bc_disconnect(conn);
+    return EXIT_SUCCESS;
 }
